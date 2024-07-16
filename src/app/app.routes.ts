2421a8c62f5d8b5128e18f8c2ec6a113ad268c47
krensiprojects <104081299+krensiprojects@users.noreply.github.com>
2024-07-16 18:19:11 +0200

    Initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,20 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'folder/inbox',
+    pathMatch: 'full',
+  },
+  {
+    path: 'folder/:id',
+    loadComponent: () =>
+      import('./folder/folder.page').then((m) => m.FolderPage),
+  },
+  {
+    path: 'loader',
+    loadChildren: () =>
+      import('./pages/loader/loader.module').then((m) => m.LoaderPageModule),
+  },
+  
+];
