29316dece67cb2cd6276425641aef0d1da85883a
krensiprojects <104081299+krensiprojects@users.noreply.github.com>
2024-11-27 14:44:51 +0100

    JPQL query

--- zahire-server/src/main/java/al/polis/zahire/repository/CatalogueProductRepository.java
+++ zahire-server/src/main/java/al/polis/zahire/repository/CatalogueProductRepository.java
@@ -1,7 +1,11 @@
 package al.polis.zahire.repository;
 
 import al.polis.zahire.model.CatalogueProduct;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +13,22 @@ import java.util.List;
 @Repository
 public interface CatalogueProductRepository extends JpaRepository<CatalogueProduct, Long> {
 
-    // Search by code
-    List<CatalogueProduct> findByDescriptionContainsIgnoreCase(String searchCriterion);
+
+    //List<CatalogueProduct> findByDescriptionContainsIgnoreCase(String searchCriterion);
+
+    // Search by description, case-insensitive
+    @Query("SELECT p FROM CatalogueProduct p WHERE LOWER(p.description) LIKE LOWER(CONCAT('%', :searchCriterion, '%'))")
+    List<CatalogueProduct> findByDescriptionContainsIgnoreCase(@Param("searchCriterion") String searchCriterion);
+
+    // Search by description with pagination and sorting
+    @Query("SELECT p FROM CatalogueProduct p WHERE LOWER(p.description) LIKE LOWER(CONCAT('%', :searchCriterion, '%'))")
+    Page<CatalogueProduct> findByDescriptionContainsIgnoreCase(@Param("searchCriterion") String searchCriterion, Pageable pageable);
+
+    // Fetch the whole catalogue
+    @Query("SELECT p FROM CatalogueProduct p")
+    List<CatalogueProduct> findTheWholeCatalogue();
+
+    // Fetch the whole catalogue with pagination and sorting
+    @Query("SELECT p FROM CatalogueProduct p")
+    Page<CatalogueProduct> findAll(Pageable pageable);
 }
