b1f7b1bbac5b6e7621066665a6606070b7519c7e
krensiprojects <104081299+krensiprojects@users.noreply.github.com>
2025-01-07 14:07:16 +0100

    CartServiceImpl

--- zahire-server/src/main/java/al/polis/zahire/service/impl/CartServiceImpl.java
+++ zahire-server/src/main/java/al/polis/zahire/service/impl/CartServiceImpl.java
@@ -1,5 +1,42 @@
 package al.polis.zahire.service.impl;
 
-public class CartServiceImpl {
-    
+import al.polis.zahire.dto.Cart.*;
+import al.polis.zahire.service.CartService;
+
+public class CartServiceImpl extends CartService {
+
+    @Override
+    public Add2CartResDto addProductToCart(Add2CartReqDto requestDto) {
+        return null;
+    }
+
+    @Override
+    public ChangeCartQtyResDto increaseCartQty(ChangeCartQtyReqDto requestDto) {
+        return null;
+    }
+
+    @Override
+    public ChangeCartQtyResDto decreaseCartQty(ChangeCartQtyReqDto requestDto) {
+        return null;
+    }
+
+    @Override
+    public RemoveFromCartResDto removeProductFromCart(RemoveFromCartReqDto requestDto) {
+        return null;
+    }
+
+    @Override
+    public ShowCartResDto showCart(ShowCartReqDto requestDto) {
+        return null;
+    }
+
+    @Override
+    public BookProdResDto bookProduct(BookProdReqDto requestDto) {
+        return null;
+    }
+
+    @Override
+    public GetOrderTotalsResDto getOrderTotals(GetOrderTotalsReqDto requestDto) {
+        return null;
+    }
 }
